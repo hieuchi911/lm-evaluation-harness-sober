ff383dfa8595f22a7ccb9c89c1b281f6457c5427
Hieu Nguyen <hieuchi911@gmail.com>
2024-08-12 16:25:34 -0700

    Annotate halu-eval script

--- hallu-eval.sh
+++ hallu-eval.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
+
+# number of gpus
 NGPUS=4
+# pairs of (where_to_save_results  absolute_paths)
 MODEL_PATHS=(
-    llama2-7b-kd  /scratch1/hieutn/ckps-selfkd-hpo-new/e3-bs8-lr5e-06-G1-N4-NN1-kd1.0-mp4/3750/
-    llama2-7b-baseline /scratch1/hieutn/ckps-sft-hpo/e3-bs4-lr5e-06-G1-N4-NN1-mp4/7500/
+    saves/llama2-7b-kd  /scratch1/hieutn/ckps-selfkd-hpo-new/e3-bs8-lr5e-06-G1-N4-NN1-kd1.0-mp4/3750/
+    saves/llama2-7b-baseline /scratch1/hieutn/ckps-sft-hpo/e3-bs4-lr5e-06-G1-N4-NN1-mp4/7500/
 )
 
 for ((i=0; i<${#MODEL_PATHS[@]}; i+=2)) do
@@ -13,7 +17,8 @@ for ((i=0; i<${#MODEL_PATHS[@]}; i+=2)) do
     ARGS+=" --tasks halueval_summarization,xsum_v2,cnndm_v2"
     ARGS+=" --batch_size auto"
     ARGS+=" --log_samples"
-    ARGS+=" --output_path saves/${SAVE_PATH}"
+    ARGS+=" --output_path ${SAVE_PATH}"
 
     echo "lm_eval ${ARGS}"
+    lm_eval ${ARGS}
 done
